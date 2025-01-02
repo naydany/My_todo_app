30943b1ac2a967be0e72cdb8962f11a28f4edfac
Dany Nay <dany.nay@axivit.com>
2025-01-02 10:34:04 +0700

    create_mytodo

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app>
+    <!-- <v-app-bar title="Application bar"></v-app-bar> -->
+    <v-toolbar>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>
+        <RouterLink to="/">Vuetify</RouterLink>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <!-- <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn> -->
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <!-- <v-btn color="primary" v-bind="props"> Activator slot </v-btn> -->
+          <v-btn icon class="text-none" stacked v-bind="props">
+            <v-badge color="red-darken-2" content="6" size="x-small">
+              <v-icon icon="mdi-bell" color="blue-darken-4"></v-icon>
+            </v-badge>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in notifications"
+            :key="index"
+            :value="index"
+          >
+            <v-list-item-title>
+              <v-icon :color="item.color" :icon="item.icon"></v-icon>
+              {{ item.title }}
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-btn
+        to="/login"
+        variant="outlined"
+        color="red-darken-4"
+        size="default"
+        text="login"
+      >
+        Login
+      </v-btn>
+    </v-toolbar>
+    <v-main>
+      <v-slide-x-transition>
+        <RouterView />
+      </v-slide-x-transition>
+    </v-main>
+  </v-app>
+</template>
+<script setup lang="js">
+import { RouterView } from "vue-router";
+
+const notifications = [
+  { title: "Messages 1", icon: "mdi-message", "color": "blue"},
+  { title: "Mesages 2", icon: "mdi-message", "color": "red"},
+  { title: "Messages 3", icon: "mdi-message", "color": "pink"},
+  { title: "Messages 4", icon: "mdi-message", "color": "yellow"},
+  { title: "Messages 5", icon: "mdi-message", "color": "orange"},
+  { title: "Messages 6", icon: "mdi-message", "color": "blue"},
+];
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container>
+    <v-card class="pa-4 mx-auto" width="400">
+      <v-card-title class="text-center">Login here</v-card-title>
+      <v-card-item>
+        <v-form @submit.prevent="submit">
+          <v-text-field
+            v-model="form.email"
+            label="Email address"
+            type="email"
+            variant="underlined"
+            append-inner-icon="mdi-email"
+          >
+          </v-text-field>
+          <v-text-field
+            v-model="form.password"
+            label="Password"
+            type="password"
+            hint="Inter your password to acccess this website"
+            variant="underlined"
+            append-inner-icon="mdi-key"
+          >
+          </v-text-field>
+          <v-checkbox
+            v-model="form.remember"
+            color="success"
+            label="Remember me"
+            value="Data has been saved"
+            hide-details
+          >
+          </v-checkbox>
+          <v-btn class="mt-2" color="blue-darken-3" type="submit" block>
+            <v-progress-circular
+              v-if="isloading"
+              color="withe"
+              indeterminate
+            ></v-progress-circular>
+            <span v-else>Submit</span>
+          </v-btn>
+        </v-form>
+      </v-card-item>
+      <v-card-action>
+        <div class="mx-4">
+          <v-btn block to="/register"> Register </v-btn>
+        </div>
+      </v-card-action>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup lang="js">
+import {ref} from "vue";
+
+const form = ref({
+  email: "",
+  password: "",
+});
+const isloading = ref(false);
+
+function submit() {
+    isloading.value = true;
+
+    setTimeout(() => {
+        isloading.value=false;
+        alert(JSON.stringify(form.value))
+    }, 3000);
+}
+</script>
